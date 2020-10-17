5d0df92734797bbcba98584c158a7750c2dc91a6
alinandrei74 <72814555+alinandrei74@users.noreply.github.com>
2020-10-17 14:10:07 +0200

    Update chatbot_básico.py

--- chatbot_básico.py
+++ chatbot_básico.py
@@ -1,3 +1,5 @@
+###   Ejercicio del curso de JetBrains Academy   ###
+
 def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
     print('I was created in ' + birth_year + '.')
